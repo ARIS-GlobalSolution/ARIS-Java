067f3012d3d91f159ef97d1fa8419453d1790cd0
Etvtaeyo <ericknakajune10@gmail.com>
2026-06-08 22:12:00 -0300

    Criando Dockerfile e docker-compose.yaml

--- aris-api/src/main/java/br/com/aris/aris_api/config/SecurityConfig.java
+++ aris-api/src/main/java/br/com/aris/aris_api/config/SecurityConfig.java
@@ -11,27 +11,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 public class SecurityConfig {
 
     @Bean
-<<<<<<< HEAD
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
-=======
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
->>>>>>> 3ece82d4d4b40682a5c4fa896c4e36022de98b9e
 
         return http
                 .csrf(csrf -> csrf.disable())
-<<<<<<< HEAD
                 .authorizeHttpRequests(auth ->
                         auth.anyRequest().permitAll()
                 )
                 .build();
-=======
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
-
-        return http.build();
->>>>>>> 3ece82d4d4b40682a5c4fa896c4e36022de98b9e
     }
 }
